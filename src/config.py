82fc4043994a7570569e720898725fa60515e160
Minseok Chu <mchu7797@gmail.com>
2024-03-12 10:10:14 +0900

    add turnstile capcha

--- src/config.py
+++ src/config.py
@@ -1,14 +1,14 @@
 DATABASE_CONFIG = {
     "connection_string": """
         DRIVER={ODBC Driver 17 for SQL Server};
-        SERVER=dmjam.net,8445;
+        SERVER=45.32.92.85,8445;
         DATABASE=O2Jam;
         UID=sa;
         PWD=DPJAM2022Server!;
         TrustServerCertificate=yes;""",
     "trade_connection_string": """
         DRIVER={ODBC Driver 17 for SQL Server};
-        SERVER=dmjam.net,8445;
+        SERVER=45.32.92.85,8445;
         DATABASE=O2JamTrade;
         UID=sa;
         PWD=DPJAM2022Server!;
